d5c41c87f5f280af07169fe893a51d3d424d0dff
andreazita <mandreazita@gmail.com>
2022-04-07 20:36:56 -0400

    Showed temperature

---
+++ src/app.js
@@ -0,0 +1,17 @@
+let apiKey = "5852a4827f249499c4c0659dbad80159";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+
+function diplayTemperature(response) {
+  console.log(response.data.wind.speed);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+  let weatherDescriptionElement = document.querySelector("#weatherDescription");
+  weatherDescriptionElement.innerHTML = response.data.weather[0].description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+axios.get(apiUrl).then(diplayTemperature);
